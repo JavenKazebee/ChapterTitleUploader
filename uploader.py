bff03c61cab8cfb40edf0bddb94f16080a5270c3
Javen Kazebee <javenkazebee@Javens-MacBook-Pro.local>
2024-04-10 17:06:23 -0700

    Untested logic implemented

--- uploader.py
+++ uploader.py
@@ -2,6 +2,7 @@ import vimeo
 import sys
 from tkinter import *
 from tkinter import ttk
+from datetime import datetime, timedelta, time
 
 # Create the API client
 client = vimeo.VimeoClient(
@@ -10,15 +11,59 @@ client = vimeo.VimeoClient(
     secret='a0cxBXh+bk51oFqxVwT5g7iN+BTY3u3MaREHh2ZrkHrLTRxpXQulQjk8QgSHqQUt6EnxvQ+ly2vctss0JA0jfuWRYCB7OoE1a/IDC5UfWWwV0rCwjvqafeIE9RtdO8ly'
 )
 
-def upload():
+def add_chapter_title(video_id, title, timecode):
+
+    print(f"Add Chapter: {video_id}, {title}, {timecode}")
     # Make the API call to vimeo
-    response = client.post("/videos/933067303/chapters",
+    response = client.post(f"/videos/{video_id}/chapters",
                        data = {
-                           "title": "Timecode Test 2",
-                           "timecode": 2
+                           "title": title,
+                           "timecode": timecode
                        })
 
-    print(response.json())
+
+def upload():
+    # Split the text into lines
+    lines = chapterTitles_txt.get("1.0", "end").split("\n")
+    # Get rid of extra trailing line
+    lines.pop()
+
+    diff_time = time(hour=0, minute=0, second=0)
+    
+    for index, line in enumerate(lines):
+        print(f"Line: {line}")
+        # Ignore first timestamp if we are converting (the "00:00 - Worship" timestamp)
+        if (not convert_timestamps.get()) or (convert_timestamps.get() and index != 0):
+            print("Passed Check")
+            # Split the line into the timestamp section and the title section: timestamp - title
+            sections = line.split("-")
+
+            # Strip whitespace from sections (mainly spaces before/after)
+            for i, section in enumerate(sections):
+                sections[i] = section.strip()
+
+            # Read and format the timestamp
+            timestamp = 0
+            format = ""
+            if(sections[0].count(":") == 2):
+                format = "%H:%M:%S"
+            else:
+                format = "%M:%S"
+
+            timestamp = datetime.strptime(sections[0], format)
+
+            # Set the difference between YT and Vimeo timestamps if we are converting timestamps
+            if convert_timestamps.get() and index == 1:
+                diff_time = timestamp
+
+            # Apply the difference
+            timestamp = timestamp - timedelta(minutes=diff_time.minute, seconds=diff_time.second)
+
+            # Convert to seconds for vimeo
+            timestamp = timestamp.hour * 3600 + timestamp.minute * 60 + timestamp.second
+
+            add_chapter_title(video_id.get(), sections[1], timestamp)
+
 
 
 # Create UI
@@ -36,6 +81,7 @@ videoID_field = ttk.Entry(frame, textvariable=video_id, width=14)
 chapterTitles_txt = Text(frame, width=40, height=20)
 
 convert_timestamps = BooleanVar()
+convert_timestamps.set(False)
 convertTimestamps_check = ttk.Checkbutton(frame, text="Convert Timestamps (for Worship vs Teaching Only)", 
                                           variable=convert_timestamps, onvalue=True, offvalue=False)
 
