eae5b693db2984391a9da579d7bfa34276307e1e
Javen Kazebee <javen.kazebee@gmail.com>
2024-04-27 17:40:26 -0700

    Better handling of API keys

--- uploader.py
+++ uploader.py
@@ -1,14 +1,17 @@
 import vimeo
-import sys
 from tkinter import *
 from tkinter import ttk
 from datetime import datetime, timedelta, time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Create the API client
 client = vimeo.VimeoClient(
-    token='c93bc5b483b3e7db712c7924d1b39464',
-    key='bc833cfdbd7c734a2a5b47053686914cf1c34b59',
-    secret='a0cxBXh+bk51oFqxVwT5g7iN+BTY3u3MaREHh2ZrkHrLTRxpXQulQjk8QgSHqQUt6EnxvQ+ly2vctss0JA0jfuWRYCB7OoE1a/IDC5UfWWwV0rCwjvqafeIE9RtdO8ly'
+    token=os.getenv("TOKEN"),
+    key=os.getenv("KEY"),
+    secret=os.getenv("SECRET")
 )
 
 def add_chapter_title(video_id, title, timecode):
